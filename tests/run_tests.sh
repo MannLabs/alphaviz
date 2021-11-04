5be0c6070eb49da193fc4ce79fce0d02e1a1aadf
EugeniaVoytik <jane.voytik@gmail.com>
2021-11-04 10:04:15 +0100

    TEST: deactivate cli and gui testing

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 conda activate alphaviz
-python -m unittest test_cli
-python -m unittest test_gui
-python -m pytest test_io
-python -m pytest test_preprocessing
+# python -m unittest test_cli
+# python -m unittest test_gui
+python -m pytest test_io.py
+python -m pytest test_preprocessing.py
 conda deactivate
