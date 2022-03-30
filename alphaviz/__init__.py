ab6232c90901dd93b292120c06d259f657772cbe
EugeniaVoytik <jane.voytik@gmail.com>
2022-03-30 09:10:51 +0200

    Bump version: 1.1.7 → 1.1.8

--- alphaviz/__init__.py
+++ alphaviz/__init__.py
@@ -1,7 +1,7 @@
 #!python
 
 __project__ = "alphaviz"
-__version__ = "1.1.7"
+__version__ = "1.1.8"
 __license__ = "Apache"
 __description__ = "A interactive Dashboard to explore mass spectrometry data."
 __author__ = "Eugenia Voytik"
