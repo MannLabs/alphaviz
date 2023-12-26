13efdd7898783fd1ba3f5b9906c3ecb8b4956e4e
jalew188 <jalew188@gmail.com>
2023-12-26 17:28:26 +0100

    Bump version: 1.2.0 → 1.3.0

--- alphaviz/__init__.py
+++ alphaviz/__init__.py
@@ -1,7 +1,7 @@
 #!python
 
 __project__ = "alphaviz"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __license__ = "Apache"
 __description__ = "A interactive Dashboard to explore mass spectrometry data."
 __author__ = "Eugenia Voytik"
