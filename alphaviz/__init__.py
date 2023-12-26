b831196516440ab767f49e009068cbfbd0e98385
jalew188 <jalew188@gmail.com>
2023-12-26 20:04:26 +0100

    FIX installer

--- alphaviz/__init__.py
+++ alphaviz/__init__.py
@@ -16,7 +16,7 @@ __keywords__ = [
     "bioinformatics",
     "visualization"
 ]
-__python_version__ = ">=3.8,<3.9"
+__python_version__ = ">=3.8,<3.12"
 __classifiers__ = [
         # "Development Status :: 1 - Planning",
         # "Development Status :: 2 - Pre-Alpha",
